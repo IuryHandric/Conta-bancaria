25e02a4c29cbc77e237085ea50309ccd99821841
Iury Handric <handric11@hotmail.com>
2025-06-01 15:49:12 -0300

    Função de Depósito

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// modulos externos
 const inquirer = require('inquirer');
 const chalk = require('chalk')
-
-
-// modulos internos
-
 const fs = require('fs');
 
 operation()
@@ -24,10 +19,141 @@ function operation() {
         }
         ])
     .then((answer) => {
-        const action = answer['action']
+        const action = answer['action'];
 
-        console.log(action);
+        if(action === 'Criar conta'){
+            createAccount();
+        } else if (action === 'Consultar Saldo'){
+
+        } else if(action === 'Depositar'){
+            deposit();
+        } else if(action === 'Sacar'){
+
+        } else if(action === 'Sair'){
+            console.log(chalk.bgBlue.black('Obrigado por usar nosso banco!'))
+            process.exit();
+        };
     })
     .catch((e) => console.log(e));
 }
 
+
+// Criando a conta
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir:'))
+    buildAccount();
+}
+
+// Formatando a conta, definindo nome
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta'
+        }
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+
+// Criando um pseudobanco para armazenar as contas recebidas
+        if(!fs.existsSync('accounts')) {
+        // Criando a pasta, caso ainda não exista
+            fs.mkdirSync('accounts')
+        }
+// Procurando se existe um arquivo com o nome da conta
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Essa conta já existe, escolha outro nome'))
+            buildAccount();
+            return;
+        }
+// Se não existir o nome da conta, criando o arquivo.json.
+    fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', (err) => {console.log(err)})
+
+    console.log(chalk.green('Parabéns, a sua conta foi criada!'));
+
+    operation();
+
+    })
+    .catch((e) => console.log(e));
+}
+
+// Depositando na conta
+
+function deposit() {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Para qual conta deseja depositar?'
+    }])
+    .then((answer) => {
+        const accountName = answer['accountName']
+        // verificando se a conta existe
+    
+    if(!checkAccount(accountName)) {
+        return deposit();
+    }
+    
+    inquirer.prompt([{
+        name: 'amount',
+        message: 'Quanto você deseja depositar?'
+    }])
+    .then((answer) => {
+
+    const amount = answer['amount']
+
+    addAmount(accountName, amount);
+    operation()
+    })
+    .catch((e) => console.log(e));
+
+    })
+    .catch((e) => console.log(e))
+}
+
+// Função para verificar se a conta existe
+
+function checkAccount(accountName) {
+    if(!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black('Essa conta não existe, escolha outra conta!'))
+        return false
+    }
+        return true
+}
+
+
+// Adicionando valores na conta via depósito
+
+function addAmount(accountName, amount) {
+
+    const accountData = getAccount(accountName)
+
+    if(!amount) {
+        console.log('Ocorreu um erro, tente novamente mais tarde!')
+        return deposit();
+    }
+
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function(err) {
+            console.log(err)
+        }    
+    )
+
+    console.log(chalk.green(`Foi depositado o valor de R$${amount},00 na sua conta!`))
+
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`,
+    {
+    encoding: 'utf8',
+    flag: 'r'
+    })
+
+    return JSON.parse(accountJSON);
+}
