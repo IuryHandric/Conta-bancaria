52a847da71473d58c58ee8eb97f22120959be34a
IuryHandric <handric11@hotmail.com>
2025-06-01 14:51:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// modulos externos
+const inquirer = require('inquirer');
+const chalk = require('chalk')
+
+
+// modulos internos
+
+const fs = require('fs');
+
+operation()
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+            'Criar conta', 
+            'Consultar Saldo', 
+            'Depositar', 
+            'Sacar', 
+            'Sair'
+        ]
+        }
+        ])
+    .then((answer) => {
+        const action = answer['action']
+
+        console.log(action);
+    })
+    .catch((e) => console.log(e));
+}
+
